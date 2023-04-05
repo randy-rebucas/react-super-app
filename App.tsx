b46334bb0016940439008ab3a8d8f8cc80caddc8
randy-rebucas <rebucasrandy1986@gmail.com>
2023-04-06 07:58:19 +0800

    added submodule

--- App.tsx
+++ App.tsx
@@ -21,23 +21,23 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import {register, addPortal, PortalView} from '@ionic/portals-react-native';
 
-// register(
-//   'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyY2QyYzExNi02ZTJmLTQ3ZDYtODYxNi1lYjEzZjE4MWI1ZGIiLCJqdGkiOiJYUjRyYkRfMmJhdHM5M1VoVGxKbkQzMC13ME9LY1Rocm5vblpBamh4VlhZIn0.mlLBVTNKLw3RkzlmywW35bKZ7EKP4hKFdL2103zkwXtEi3hLiyxdBSFZkcYw2bpScsqu0wyFT39CSYe9XLgJSzpt0O9YdyOr95h5a_q0_DedVys7CC_3_soP0MHSQeX7VCN65KP0d9AuhoRG-tgANVAcbhsmocQk8Yobyki0ATjqXZdF-E5jEo0QggeXAUjZXXlzSC2KUsN5l0dZNrOyKMs1E6jYR-u78C_AGmcljaANqlqtY0E7h2sP8QhEY0Oe-fTFBUcaghu6RCV-bzb-dvy2Hk1eNzxpKHLPfb72-rD0qgPeAEt4F3x99I-TMUX9ARB6Y_1z51bBfANgq21Gpw',
-// );
+register(
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyY2QyYzExNi02ZTJmLTQ3ZDYtODYxNi1lYjEzZjE4MWI1ZGIiLCJqdGkiOiJYUjRyYkRfMmJhdHM5M1VoVGxKbkQzMC13ME9LY1Rocm5vblpBamh4VlhZIn0.mlLBVTNKLw3RkzlmywW35bKZ7EKP4hKFdL2103zkwXtEi3hLiyxdBSFZkcYw2bpScsqu0wyFT39CSYe9XLgJSzpt0O9YdyOr95h5a_q0_DedVys7CC_3_soP0MHSQeX7VCN65KP0d9AuhoRG-tgANVAcbhsmocQk8Yobyki0ATjqXZdF-E5jEo0QggeXAUjZXXlzSC2KUsN5l0dZNrOyKMs1E6jYR-u78C_AGmcljaANqlqtY0E7h2sP8QhEY0Oe-fTFBUcaghu6RCV-bzb-dvy2Hk1eNzxpKHLPfb72-rD0qgPeAEt4F3x99I-TMUX9ARB6Y_1z51bBfANgq21Gpw',
+);
 
-// const helloPortal = {
-//   // A unique name to reference later
-//   name: 'hello',
-//   // This is the location of your web bundle relative to the asset directory in Android and Bundle.main in iOS
-//   // This will default to the name of the portal
-//   startDir: 'portals/hello',
-//   // Any initial state to be provided to a Portal if needed
-//   initialContext: {
-//     greeting: 'Hello, world!',
-//   },
-// };
+const helloPortal = {
+  // A unique name to reference later
+  name: 'hello',
+  // This is the location of your web bundle relative to the asset directory in Android and Bundle.main in iOS
+  // This will default to the name of the portal
+  startDir: 'portals/hello',
+  // Any initial state to be provided to a Portal if needed
+  initialContext: {
+    greeting: 'Hello, world!',
+  },
+};
 
-// addPortal(helloPortal);
+addPortal(helloPortal);
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
